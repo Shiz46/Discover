b02337c2786c038bd0eb684ea49c69ce9f8f0452
Shizue Chalwell <shizue46@gmail.com>
2019-08-07 03:05:49 +0000

    caption

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_05_030318) do
+ActiveRecord::Schema.define(version: 2019_08_07_024223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_08_05_030318) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.text "caption"
   end
 
   create_table "places", force: :cascade do |t|
