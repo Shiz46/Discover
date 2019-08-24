4c127d3be635bfd5a5a2688ade7a62d0b2efae08
Shizue Chalwell <shizue46@gmail.com>
2019-08-24 03:09:41 +0000

    route issue

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,15 +4,15 @@ class PhotosController < ApplicationController
   def create
     @place = Place.find(params[:place_id])
     #@place.photos.create(photo_params)
-    @place.photos.create(photo_params.merge(user: current_user))
+    @place.captions.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
     # can I used merge to connect Place_Id?
   end
 
   private 
 
-  def photo_params 
-    params.require(:caption).permit(:photo)
+  def caption_params 
+    params.require(:caption).permit(:caption)
   end
 end
 
