ccbb9349d6b75f9e368ebe518fbe6a400910b9ac
Shizue Chalwell <shizue46@gmail.com>
2019-09-05 16:23:35 +0000

    Delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # run rake db:seeds
+    usr = User.create({:email => "guy@gmail.com", :password => "111111", :password_confirmation => "111111" })
+
+  Place.create(user_id: usr.id, name: 'Calabash', description: 'Great Caribbean cuisine.', address: ' 428 Carrall St, Vancouver, BC V6A0A7')
+
+  Place.create(user_id: usr.id , name: 'Trattoria', description: 'Great Caribbean cuisine.', address: ' 428 Carrall St, Vancouver, BC V6A0A7')
+
+  Place.create(user_id: usr.id , name: 'Keg Steak House', description: 'Great Caribbean cuisine.', address: ' 428 Carrall St, Vancouver, BC V6A0A7')
+
+  Place.create(user_id: usr.id , name: 'Richmond Night Market', description: 'Great Caribbean cuisine.', address: ' 428 Carrall St, Vancouver, BC V6A0A7')
+
+  Place.create(user_id: usr.id , name: 'White Spot', description: 'Great Caribbean cuisine.', address: ' 428 Carrall St, Vancouver, BC V6A0A7')
